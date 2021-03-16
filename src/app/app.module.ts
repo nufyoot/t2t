e3062fc3e91833c2f993544b51de55b1edc6d346
Terry Tice <nufyoot@gmail.com>
2021-03-15 21:17:53 -0400

    Checkpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputBarComponent } from './input-bar/input-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputBarComponent
   ],
   imports: [
     BrowserModule,
